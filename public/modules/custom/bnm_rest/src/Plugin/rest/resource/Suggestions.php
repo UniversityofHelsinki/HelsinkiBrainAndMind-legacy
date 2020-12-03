05651403932c3e095f90445e39328c5d84e37e31
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-12-03 10:59:12 +0200

    rest endpoint for suggestions

---
+++ public/modules/custom/bnm_rest/src/Plugin/rest/resource/Suggestions.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\bnm_rest\Plugin\rest\resource;
+
+use Drupal\search_api\Entity\Index;
+use Drupal\search_api_autocomplete\Entity\Search;
+use Drupal\rest\Plugin\ResourceBase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Provides a resource to get available filters.
+ *
+ * @RestResource(
+ *   id = "filter_rest_resource",
+ *   label = @Translation("Suggestions"),
+ *   uri_paths = {
+ *     "canonical" = "/suggestions",
+ *     "https://www.drupal.org/link-relations/create" = "/filters"
+ *   }
+ * )
+ */
+final class Suggestions extends ResourceBase {
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\taxonomy\Entity\TermStorageInterface $manager
+   *   The term manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+  }
+
+  /**
+   * Create.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      \Drupal::logger('bnm')
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The HTTP response object.
+   */
+  public function get(Request $request) {
+    $q = $request->get('q');
+
+    if(!$q){
+      return new JsonResponse([]);
+    }
+
+    /** @var \Drupal\search_api_autocomplete\Utility\PluginHelper $plugin_helper */
+    $plugin_helper = \Drupal::service('search_api_autocomplete.plugin_helper');
+
+    $search = Search::create(Search::getDefaultOptions());
+
+    /** @var \Drupal\search_api_autocomplete\Plugin\search_api_autocomplete\suggester\Server $suggester */
+    $suggester = $plugin_helper->createSuggesterPlugin($search, 'server', []);
+
+    $index = Index::load('research_group');
+    $query = $index->query([])->range(0, 5);
+
+    $incomplete_key = $q;
+    $user_input = $q;
+
+    $suggestions = $suggester->getAutocompleteSuggestions($query, $incomplete_key, $user_input);
+
+    $return = [];
+    foreach($suggestions as $suggestion){
+      $return[] = "{$suggestion->getUserInput()}{$suggestion->getSuggestionSuffix()}";
+    }
+    return new JsonResponse($return);
+  }
+
+}
