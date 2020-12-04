4ef5151a0c2d9da8beab61ddc36fce4780ee7744
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-12-04 08:28:22 +0200

    vue init

---
+++ public/modules/custom/bnm_frontend/js/search/src/components/Results/ResultList.vue
@@ -0,0 +1,17 @@
+<template>
+    <Result/>
+</template>
+
+<script>
+  import Result from './Result.vue';
+
+  export default {
+    name: 'ResultList',
+    components: {
+      Result,
+    },
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ public/modules/custom/bnm_frontend/js/search/src/components/SearchBar/Dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <select>
+            <option value="1">first option</option>
+        </select>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dropdown',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
