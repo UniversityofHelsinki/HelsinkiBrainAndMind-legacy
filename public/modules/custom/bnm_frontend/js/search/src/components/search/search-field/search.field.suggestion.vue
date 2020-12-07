2fb25f0ffd061ee07aefaebcf179b8e03e056da1
shamalainen <sebastian@hamse.fi>
2020-12-07 15:39:11 +0200

    Add search field suggestion component with skeleton structure

---
+++ public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.suggestion.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="search-suggestions">
+    <li class="search-suggestions__item"><button class="search-suggestions__button">Click me</button></li>
+    <li class="search-suggestions__item"><button class="search-suggestions__button">Click me</button></li>
+    <li class="search-suggestions__item"><button class="search-suggestions__button">Click me</button></li>
+    <li class="search-suggestions__item"><button class="search-suggestions__button">Click me</button></li>
+    <li class="search-suggestions__item"><button class="search-suggestions__button">Click me</button></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'SearchFieldSuggestions'
+}
+</script>
--- public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
+++ public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
@@ -3,14 +3,17 @@
     <div class="search-field">
       <label for="keywords" class="search-field__label">Keywords</label>
       <input type="text" id="keywords" class="search-field__input">
+      <SearchFieldSuggestions></SearchFieldSuggestions>
     </div>
   </div>
 </template>
 
 <script>
 import './search-field.scss';
+import SearchFieldSuggestions from './search.field.suggestion.vue';
 
 export default {
+  components: { SearchFieldSuggestions },
   name: 'SearchField'
 }
 </script>
