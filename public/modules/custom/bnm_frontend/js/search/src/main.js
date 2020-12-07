1a6f6eba2752c5482bef6774e02b03601dd2d0bc
shamalainen <sebastian@hamse.fi>
2020-12-07 14:09:27 +0200

    Install modern-normalize

--- public/modules/custom/bnm_frontend/js/search/src/main.js
+++ public/modules/custom/bnm_frontend/js/search/src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import '../node_modules/modern-normalize/modern-normalize.css';
+
 
 const app = createApp(App)
   .use(VueAxios, axios);
 
-axios.defaults.headers.common['Content-Type'] = 'application/json';
 app.config.globalProperties.axios = axios;
 
 app.mount('#app');
