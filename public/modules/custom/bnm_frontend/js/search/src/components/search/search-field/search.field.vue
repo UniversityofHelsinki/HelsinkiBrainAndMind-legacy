7dc4952b5925fc65eaebcee00661c55b7c4c38fe
shamalainen <sebastian@hamse.fi>
2020-12-07 16:11:37 +0200

    Create search keywords component

--- public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
+++ public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="search">
+  <div class="search__item">
     <div class="search-field">
       <label for="keywords" class="search-field__label">Keywords</label>
       <input type="text" id="keywords" class="search-field__input">
       <SearchFieldSuggestions></SearchFieldSuggestions>
     </div>
+    <SearchFieldKeywords></SearchFieldKeywords>
   </div>
 </template>
 
 <script>
 import './search-field.scss';
-import SearchFieldSuggestions from './search.field.suggestion.vue';
+import SearchFieldSuggestions from './search.field.suggestions.vue';
+import SearchFieldKeywords from './search.field.keywords.vue';
 
 export default {
-  components: { SearchFieldSuggestions },
+  components: { SearchFieldSuggestions, SearchFieldKeywords },
   name: 'SearchField'
 }
 </script>
