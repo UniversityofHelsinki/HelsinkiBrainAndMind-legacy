c56fd2264569660ba298ab0c0e7a0b3648bb534f
shamalainen <sebastian@hamse.fi>
2020-12-08 13:58:58 +0200

    Add keyword add functionality

---
+++ public/modules/custom/bnm_frontend/js/search/src/components/search/search-keywords/search.keywords.item.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="search-keywords__item">
+    <button class="search-keywords__button">
+      <slot />
+      <span>
+        <IconClear></IconClear>
+      </span>
+    </button>
+  </li>
+</template>
+
+<script>
+import IconClear from '../../../assets/icons/icon--clear.vue';
+
+export default {
+  name: 'SearchKeywordsItem',
+  components: { IconClear }
+}
+</script>
