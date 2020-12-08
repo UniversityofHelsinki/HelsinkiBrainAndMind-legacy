7ed9aac2bc73a4b750ca027c442d4b45c5ddf040
shamalainen <sebastian@hamse.fi>
2020-12-08 13:14:05 +0200

    Populate suggestions popup with real data

--- public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
+++ public/modules/custom/bnm_frontend/js/search/src/components/search/search-field/search.field.vue
@@ -2,8 +2,8 @@
   <div>
     <div class="search-field">
       <label for="keywords" class="search-field__label">Keywords</label>
-      <input type="text" id="keywords" class="search-field__input">
-      <SearchFieldSuggestions></SearchFieldSuggestions>
+      <input type="text" id="keywords" :value="inputKeywordsValue" class="search-field__input" placeholder="Search ..." @input="handleSuggestions">
+      <SearchFieldSuggestions :class="{ 'is-open': suggestions.length > 0 }" :suggestions="this.suggestions"></SearchFieldSuggestions>
     </div>
     <SearchFieldKeywords></SearchFieldKeywords>
   </div>
@@ -16,6 +16,35 @@ import SearchFieldKeywords from './search.field.keywords.vue';
 
 export default {
   components: { SearchFieldSuggestions, SearchFieldKeywords },
-  name: 'SearchField'
+  name: 'SearchField',
+  data(){
+    return {
+      suggestions: [],
+      inputKeywordsValue: '',
+    }
+  },
+  methods: {
+    handleInputValueChange(keyword) {
+      this.$emit('handleUpdate', keyword);
+    },
+    handleSuggestions(event) {
+      const { value: keyword } = event.target;
+      this.handleInputValueChange(keyword);
+
+      // TODO add throttling
+      if (keyword.length > 2) {
+        this.axios.get(`https://bnm.docker.sh/search_suggestions?q=${keyword}`, {}, {
+          headers: {
+            'Content-type': 'application/json',
+          },
+        })
+        .then(({data: results}) => {
+            this.suggestions = results;
+        })
+
+        this.inputKeywordsValue = keyword;
+      }
+    }
+  }
 }
 </script>
