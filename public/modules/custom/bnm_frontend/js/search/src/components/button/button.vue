0a0c9cff8bb045ec3b6d1697c14830b4ac0da42d
shamalainen <sebastian@hamse.fi>
2020-12-08 10:44:22 +0200

    Add handleClick methods for button component

--- public/modules/custom/bnm_frontend/js/search/src/components/button/button.vue
+++ public/modules/custom/bnm_frontend/js/search/src/components/button/button.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="button">
-    Click me
+  <button @click="handleClick" class="button" :class="{ 'button--primary': isPrimary, 'button--secondary': isSecondary }" type="button">
+    <slot/>
   </button>
 </template>
 
@@ -8,6 +8,15 @@
 import './button.scss';
 
 export default {
-  name: 'Button'
+  name: 'Button',
+  props: {
+    isPrimary: Boolean,
+    isSecondary: Boolean
+  },
+  methods: {
+    handleClick(event){
+      this.$emit('handleClick', event);
+    }
+  },
 }
 </script>
