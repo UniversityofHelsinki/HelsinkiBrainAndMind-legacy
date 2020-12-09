e8389de5d14f8b14d9b2f8ef31fab5e7b5fa8b2a
shamalainen <sebastian@hamse.fi>
2020-12-09 16:53:07 +0200

    Add "no results" heading

--- public/modules/custom/bnm_frontend/js/search/src/pages/SearchPage.vue
+++ public/modules/custom/bnm_frontend/js/search/src/pages/SearchPage.vue
@@ -1,5 +1,8 @@
 <template>
     <Search @searchCompleted="getSearchResults" @searchReseted="resetSearch" @isLoading="setLoadingStatus"></Search>
+    <Container v-if="results.length === 0">
+      <h2>No results.</h2>
+    </Container>
     <Loader v-if="this.isLoading"></Loader>
     <SearchResults :results="results" v-if="!this.isLoading"></SearchResults>
 </template>
@@ -8,13 +11,15 @@
 import Search from '../components/search/search.vue'
 import SearchResults from '../components/search-results/search-results.vue';
 import Loader from '../components/loader/loader.vue';
+import Container from '../components/container/container.vue';
 
 export default {
   name: 'SearchPage',
   components: {
     Search,
     SearchResults,
-    Loader
+    Loader,
+    Container
   },
   data() {
     return {
