08d6696dc69a59bd0925710f56a0eb3fcba025af
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2021-01-18 16:42:06 +0200

    added misc files

---
+++ public/modules/custom/bnm_frontend/js/search/src/assets/texts/text--organizations-help.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>You can search researchers in all three organisations (University of Helsinki, Aalto University and HUS) simultaneously by default. You can also search researchers in a specific organisation by selecting the entire organisation or a specific unit from the list.</p>
+</template>
---
+++ public/modules/custom/bnm_frontend/js/search/src/components/info-icon/info-icon.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="info-icon">
+    <button :aria-label="getName()" :onfocus="setAriaAttributes" :onblur="removeAriaAttributes" :onmouseover="setAriaAttributes" :onmouseleave="removeAriaAttributes" class="info-icon__button" aria-expanded="false" :aria-describedby="this.getId()">
+      <component :is="icon"></component>
+    </button>
+    <component class="info-icon__description" :id="this.getId()" role="tooltip" :is="text" ></component>
+  </div>
+</template>
+
+<script>
+  import './info-icon.scss';
+  import Question from '../../assets/icons/icon--questionmark.vue';
+  import TextKeywordHelp from '../../assets/texts/text--keyword-help';
+  import TextOrganizationsHelp from '../../assets/texts/text--organizations-help';
+
+  export default {
+    name: 'InfoIcon',
+    components: {
+      Question,
+      TextKeywordHelp,
+      TextOrganizationsHelp
+    },
+    methods: {
+      getId(){
+        return this.text;
+      },
+      getName(){
+        return this.iconName;
+        //return `How to use ${this.iconName}`;
+      },
+      setAriaAttributes(event){
+        event.target.setAttribute('aria-expanded', 'true')
+      },
+      removeAriaAttributes(event){
+        event.target.setAttribute('aria-expanded', 'false')
+      }
+    },
+    props: {
+      icon: {
+        type: String,
+        default: 'Question'
+      },
+      text: {
+        type: String,
+        default: 'TextKeywordHelp',
+      },
+      iconName: String,
+    }
+  }
+</script>
