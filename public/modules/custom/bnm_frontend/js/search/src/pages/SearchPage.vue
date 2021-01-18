c992b1589a6c3ef262834efd99f3d6bf07bc9418
shamalainen <sebastian@hamse.fi>
2021-01-18 11:43:26 +0200

    Initialize pager component with state logic

--- public/modules/custom/bnm_frontend/js/search/src/pages/SearchPage.vue
+++ public/modules/custom/bnm_frontend/js/search/src/pages/SearchPage.vue
@@ -5,6 +5,7 @@
     </Container>
     <Loader v-if="this.isLoading"></Loader>
     <SearchResults :results="results" v-if="!this.isLoading"></SearchResults>
+    <Pager v-if="isPagerVisible && results.length !== 0"></Pager>
     <Footer></Footer>
 </template>
 
@@ -14,6 +15,7 @@ import SearchResults from '../components/search-results/search-results.vue';
 import Loader from '../components/loader/loader.vue';
 import Container from '../components/container/container.vue';
 import Footer from '../components/footer/footer.vue';
+import Pager from '../components/pager/pager.vue'
 import useInitialData from '../stores/data';
 import { watch } from '@vue/runtime-core';
 
@@ -25,11 +27,13 @@ export default {
     Loader,
     Container,
     Footer,
+    Pager
   },
   data() {
     return {
       results: [],
       isLoading: false,
+      isPagerVisible: false,
     }
   },
   methods: {
@@ -44,9 +48,27 @@ export default {
     const { fetchInitialData, setResultsLoadingStatus, results } = useInitialData();
 
     watch(() => {
-      this.getSearchResults(results._object.results);
+      const hasSubArray = results._object.results.some(item => Array.isArray(item))
+
+      this.isPagerVisible = false;
+
+      if (hasSubArray) {
+        this.getSearchResults(results._object.results[results._object.currentPage])
+        this.isPagerVisible = true;
+      } else {
+        this.getSearchResults(results._object.results);
+      }
+
+    if (results._object.pageCount === 0) {
+      this.isPagerVisible = false;
+    }
+
       this.setLoadingStatus(results._object.resultsLoadingStatus);
     });
+
+    if (results._object.pageCount === 0) {
+      this.isPagerVisible = false;
+    }
 
     setResultsLoadingStatus();
 
--- public/modules/custom/bnm_frontend/js/search/src/stores/data.js
+++ public/modules/custom/bnm_frontend/js/search/src/stores/data.js
@@ -1,12 +1,15 @@
 import { reactive, toRefs } from 'vue';
 // eslint-disable-next-line
 import { initialDataRequest, searchResultRequest } from '../services/request-helper';
+import chunkArray from '../utils/chunkArray';
 
 const state = reactive({
   affiliates: [],
   footer_menu: [],
   results: [],
   resultsLoadingStatus: false,
+  currentPage: 0,
+  pageCount: 0,
 });
 
 export default function useInitialData(environment){
@@ -19,6 +22,8 @@ export default function useInitialData(environment){
       state.footer_menu = response.data.footer_menu;
       state.results = response.data.initial_search_results;
       state.resultsLoadingStatus = false;
+      state.currentPage = 0;
+      state.pageCount = 0;
     })
     .catch((error) => {
       return error
@@ -29,9 +34,10 @@ export default function useInitialData(environment){
     searchResultRequest(endpoint, query, environment)
     .then((response) => {
       setResultsLoadingStatus();
-      state.results = response.data;
+      state.pageCount = chunkArray(response.data, 20).length - 1;
+      state.results = chunkArray(response.data, 20);
     })
-    .catch(() => {
+    .catch((error) => {
       // eslint-disable-next-line
       console.log(error);
     });
@@ -45,11 +51,23 @@ export default function useInitialData(environment){
     state.resultsLoadingStatus = !state.resultsLoadingStatus;
   };
 
+  const setPagerNextPage = () => {
+    if (state.currentPage === state.pageCount) return;
+    state.currentPage = state.currentPage + 1;
+  }
+
+  const setPagerPreviousPage = () => {
+    if (state.currentPage === 0) return;
+    state.currentPage = state.currentPage - 1;
+  }
+
   return {
     ...toRefs(state),
     fetchInitialData,
     fetchResults,
     resetResults,
-    setResultsLoadingStatus
+    setResultsLoadingStatus,
+    setPagerNextPage,
+    setPagerPreviousPage
   }
 }
