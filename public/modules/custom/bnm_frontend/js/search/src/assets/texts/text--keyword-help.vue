b179e813f48885f04a93c1af9147bf6f15a08528
shamalainen <sebastian@hamse.fi>
2021-01-21 15:16:35 +0200

    Update text for keyword helper

--- public/modules/custom/bnm_frontend/js/search/src/assets/texts/text--keyword-help.vue
+++ public/modules/custom/bnm_frontend/js/search/src/assets/texts/text--keyword-help.vue
@@ -1,3 +1,3 @@
 <template>
-  <p>You can enter any keyword related to research areas, topics, methods and techniques. The search tool will suggest keywords that are found in the database. You can also search researchers by their name.</p>
+  <p>You can enter any keyword related to research areas, topics, methods and techniques. The search tool will suggest keywords that are found in the database. You can also use multiple keywords and search researchers by their name.</p>
 </template>
