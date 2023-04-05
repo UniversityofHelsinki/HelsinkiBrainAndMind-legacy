3c01ff4b0be396d31504321a0a57babe95c8a9a8
Kirsi Jokela <kirsi.jokela@druid.fi>
2023-04-05 15:48:34 +0300

    requested changes: hover class within the input checkbox in sass, centered label on checkbox, labels and id have dashes instead of spaces

--- public/modules/custom/bnm_frontend/js/search/src/components/collaboration-filter/collaboration.filter.vue
+++ public/modules/custom/bnm_frontend/js/search/src/components/collaboration-filter/collaboration.filter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="industrial-collaboration-container">
-    <input id="only industrial collaboration" class="industrial-collaboration-checkbox" type="checkbox" @change="handleCollaborationCheckbox" />
-    <label for="only industrial collaboration" class="search-field__label"> Only Industrial Collaboration </label>
+    <input id="only-industrial-collaboration" class="industrial-collaboration-checkbox" type="checkbox" @change="handleCollaborationCheckbox" />
+    <label for="only-industrial-collaboration" class="search-field__label"> Only Industrial Collaboration </label>
   </div>
 </template>
 
