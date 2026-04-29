ba63cb6da72fd86c63875ba0fbc4552cf9ea01ef
Marko Korhonen <marko.korhonen@druid.fi>
2026-04-29 15:57:46 +0300

    Set config_sync_directory

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -860,6 +860,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 
+$settings['config_sync_directory'] = '../conf/cmi';
+
 // Automatically generated include for settings managed by ddev.
 $ddev_settings = __DIR__ . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
